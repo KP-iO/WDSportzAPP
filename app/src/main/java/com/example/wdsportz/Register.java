984d27659c426428f57b789644713d6e82ee3ec0
Khrishawn <khrishawnpowell@gmail.com>
2019-07-10 10:07:06 +0100

    Manifest

--- app/src/main/java/com/example/wdsportz/Register.java
+++ app/src/main/java/com/example/wdsportz/Register.java
@@ -14,7 +14,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.LoginPage);
+        setContentView(R.layout.loginpage);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
