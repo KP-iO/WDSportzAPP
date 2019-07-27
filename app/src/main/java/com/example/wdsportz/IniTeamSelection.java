5bcd3bd9896d5bd5fcca2f4afa3b53457b34b0c3
Khrishawn <khrishawnpowell@gmail.com>
2019-07-27 14:17:55 +0100

    Nav Bar and Nav Bar fragments functionality compleeted

---
+++ app/src/main/java/com/example/wdsportz/IniTeamSelection.java
@@ -0,0 +1,15 @@
+package com.example.wdsportz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IniTeamSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selectteams);
+
+    }
+};
