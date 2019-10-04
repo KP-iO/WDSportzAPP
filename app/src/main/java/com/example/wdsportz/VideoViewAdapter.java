818d7035719e45177fc18df81a4dfcf4d0df74cc
Khrishawn <khrishawnpowell@gmail.com>
2019-10-04 14:23:35 +0100

    The set up of Watch page adapter/viewmodel
    Signed-off-by: Khrishawn <khrishawnpowell@gmail.com>

---
+++ app/src/main/java/com/example/wdsportz/VideoViewAdapter.java
@@ -0,0 +1,83 @@
+package com.example.wdsportz;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class VideoViewAdapter extends RecyclerView.Adapter<VideoViewAdapter.ViewHolder> {
+
+        private ArrayList<RecyclerViewModel> mData;
+        private LayoutInflater mInflater;
+        private ItemClickListener mClickListener;
+
+        // data is passed into the constructor
+        VideoViewAdapter(Context context, ArrayList<RecyclerViewModel> data) {
+            this.mInflater = LayoutInflater.from(context);
+            this.mData = data;
+        }
+
+        // inflates the row layout from xml when needed
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.fragment_mainfeed_row, parent, false);
+            return new ViewHolder(view);
+        }
+
+        // binds the data to the TextView in each row
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.myTextView.setText(mData.get(position).getName());
+            holder.myImageView.setImageResource(mData.get(position).getImage_drawable());
+
+        }
+
+        // total number of rows
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+
+        // stores and recycles views as they are scrolled off screen
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView myTextView;
+            ImageView myImageView;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                myTextView = itemView.findViewById(R.id.textView2);
+                myImageView = itemView.findViewById(R.id.imageView2);
+
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+                if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+
+        // convenience method for getting data at click position
+       // String getItem(int id) {
+         //   return mData.get(id);
+       // }
+
+        // allows clicks events to be caught
+        void setClickListener(ItemClickListener itemClickListener) {
+            this.mClickListener = itemClickListener;
+        }
+
+        // parent activity will implement this method to respond to click events
+        public interface ItemClickListener {
+            void onItemClick(View view, int position);
+        }
+    }
+
