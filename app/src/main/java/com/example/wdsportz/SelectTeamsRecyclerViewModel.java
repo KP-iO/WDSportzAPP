2c8f89456e4bcac3be461d7401c0ebe6eec78dd6
Khrishawn <khrishawnpowell@gmail.com>
2019-10-04 14:24:47 +0100

    View model/adapter
    need to create main class

---
+++ app/src/main/java/com/example/wdsportz/SelectTeamsRecyclerViewModel.java
@@ -0,0 +1,20 @@
+package com.example.wdsportz;
+
+public class SelectTeamsRecyclerViewModel {
+
+    String teamName;
+    String teamLogoURl;
+
+    SelectTeamsRecyclerViewModel (String teamName , String teamLogoURl){
+
+        this.teamName = teamName;
+        this.teamLogoURl = teamLogoURl;
+
+
+    }
+
+
+
+
+
+}
